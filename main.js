945f34f79df8b553f70817b62d9583fbdd11d56f
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-05-06 10:37:18 +0530

    delete commit correction

--- main.js
+++ main.js
@@ -18,32 +18,35 @@ function onSubmit(e) {
     setTimeout(() => msg.remove(), 3000);
   } else {
     const li = document.createElement('li');
-
-    let userinput={"name":nameInput.value,"mobile":mobileInput.value,"email":emailInput.value};
+    const name=nameInput.value;
+    const email=emailInput.value;
+    const mobile=mobileInput.value;
+    const userinput={
+      name,
+      mobile,
+      email
+    };
+    localStorage.setItem(userinput.email,JSON.stringify(userinput));
+    showuser(userinput);
     
-    li.appendChild(document.createTextNode(`${nameInput.value}: ${mobileInput.value} :${emailInput.value}`));
-    let userinput_serialized=JSON.stringify(userinput);
-    localStorage.setItem(emailInput.value,userinput_serialized)
-    
-    userList.appendChild(li);
-    
-    var deleteBtn=document.createElement('button');
-    deleteBtn.className='delete';
-    deleteBtn.appendChild(document.createTextNode('Delete'));
-    li.appendChild(deleteBtn);
-    
-    userList.appendChild(li);
-    // var itemList=document.getElementById('users');
-    userList.addEventListener('click',removeItem);
-    function removeItem(e){
-      if(e.target.classList.contains('delete')){
-        var li=e.target.parentElement;
-        localStorage.removeItem(userinput.email);
-        userList.removeChild(li);
-      }
-    }
     nameInput.value = '';
     mobileInput.value='';
     emailInput.value = '';
   }
+}
+
+function showuser(obj){
+  const parentitem=document.getElementById("users");
+  const childitem=document.createElement("li");
+  childitem.textContent=obj.name+ ":"+obj.mobile+":"+obj.email;
+
+  const deleteitem =document.createElement("input");
+  deleteitem.type="button";
+  deleteitem.value="delete";
+  deleteitem.onclick=()=>{
+    localStorage.removeItem(obj.email);
+    parentitem.removeChild(childitem);
+  }
+  childitem.appendChild(deleteitem);
+  parentitem.appendChild(childitem);
 }
